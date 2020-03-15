bdc7406f212fba1e5b192671fae9997e5b0c09bd
Alex Hollingsworth <hollinal@indiana.edu>
2020-03-15 06:46:00 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
-# coronoavirus-simple-time-series
+# Coronavirus, simple time series
  create a simple time series of cases and deaths using coronavirus R package and  johns hopkins data 
+
+This is a simple R code to replicate a figure I've seen circulated on-line. A comparison of confirmed coronavirus cases to Italy where the date is `days since 100 confirmed cases`. Note: confirmed cases is certainly an undercount relative to true cases. See link at bottom of read me for more. 
+
+<figure style="float:left;">
+<img src="https://github.com/hollina/coronoavirus-simple-time-series/blob/master/output/coronavirus_cases_compared_to_italy.png" align="left"  width="400"  /> 
+</figure>
+
+## Source
+
+This code uses the `coronavirus` package written by RamiKrispin and available here: https://github.com/RamiKrispin/coronavirus
+
+This package pulls data from the Johns Hopkins University Center for Systems Science and Engineering (JHU CCSE): https://coronavirus.jhu.edu
+
+## Deaths
+
+The data also include deaths, which have less of a clear overlap with Italy. It is likely this death count does not capture all coronavirus related deaths. 
+
+<figure style="float:left;">
+<img src="https://github.com/hollina/coronoavirus-simple-time-series/blob/master/output/coronavirus_deaths_compared_to_italy.png" align="left"  width="400"  /> 
+</figure>
+
+## True v confirmed cases
+
+Link for information on true v confirmed cases (I have not verified accuracy of graphs/figures here): https://medium.com/@tomaspueyo/coronavirus-act-today-or-people-will-die-f4d3d9cd99ca 
+
+The basic idea is that true infections are not detected until roughly 10 to 14 days after they occur. 
+
+<figure style="float:left;">
+<img src="https://miro.medium.com/max/7168/1*r-ddYhoUtP_se6x-NOEinA.png" align="left"  width="400"  /> 
+</figure>
+
+## License
+
+This github repo: [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
---
+++ scripts/coronavirus.R
@@ -0,0 +1,104 @@
+#########################################################################################################
+# load the necessary packages
+if (!require(pacman)) {
+  install.packages("pacman")
+  library(pacman)
+}
+
+p_load(ggrepel, tidyverse)
+p_load_gh("RamiKrispin/coronavirus")
+options("tidylog.display" = NULL)
+
+#########################################################################################################
+# Load the data
+data("coronavirus")
+
+# Export the data
+write_csv(coronavirus, "data/coronavirus_data.csv")
+
+#########################################################################################################
+# Create a running count of confimred cases, deaths, and recoveries by country-day.
+analysis <- coronavirus %>% 
+  group_by(Country.Region, date, type) %>%
+  summarise(total_cases = sum(cases)) %>%
+  pivot_wider(names_from = type, values_from = total_cases) 
+
+analysis <- analysis %>%
+  group_by(Country.Region) %>%
+  mutate(total_to_date_confirmed = cumsum(confirmed)) %>%
+  mutate(total_to_date_deaths = cumsum(death)) %>%
+  mutate(total_to_date_recoveries= cumsum(recovered)) %>%
+  select(-c(confirmed, death, recovered)) %>%
+  filter(total_to_date_confirmed >= 100) 
+
+
+# Drop China and Cruise ship. Add days since 100
+analysis <- analysis %>%
+  filter(Country.Region != "China") %>%
+  filter(Country.Region != "Cruise Ship") %>%
+  group_by(Country.Region) %>%
+  mutate(threshold_date = min(date)) %>%
+  mutate(days_since = date - threshold_date) %>%
+  mutate(max_days = max(days_since)) %>%
+  filter(max_days >= 10) %>% 
+  arrange(desc(Country.Region))
+
+# Make a separate italy dataset (it will be reference line)
+italy <- analysis %>%
+  filter(Country.Region == "Italy") 
+
+#########################################################################################################
+# Cases: Make time trend of each country v italy on separate plot
+cases_v_italy <- ggplot(data = subset(analysis, Country.Region != "Italy"), aes(x = days_since, y = total_to_date_confirmed)) +
+    annotate(geom='line', x=italy$days_since,y=italy$total_to_date_confirmed, linetype = "dashed") +
+    geom_line(alpha = 1, size = 2, color = "black") +
+    geom_line(alpha = 1, size = 1.5, color = "orange") +
+    theme_classic() +
+    facet_wrap(vars(Country.Region)) +
+    ggtitle("The US, Western Europe, and Iran are on similar coronavirus case trajectories as Italy (dashed-line)") +
+    ylab("Total confirmed cases (certainly an undercount)") +
+    xlab("Days since 100 confirmed cases")
+
+ggsave(file = "output/coronavirus_cases_compared_to_italy.png", plot = cases_v_italy, height = 6, width = 9, dpi = 600)
+
+
+# Deaths: Make time trend of each country v italy on separate plot
+deaths_v_italy <- ggplot(data = subset(analysis, Country.Region != "Italy"), aes(x = days_since, y = total_to_date_deaths)) +
+  annotate(geom='line', x=italy$days_since,y=italy$total_to_date_deaths, linetype = "dashed") +
+  geom_line(alpha = 1, size = 2, color = "black") +
+  geom_line(alpha = 1, size = 1.5, color = "red") +
+  theme_classic() +
+  facet_wrap(vars(Country.Region)) +
+  ggtitle("The trajectory for deaths is more unclear. Mostly because deaths counts are still low in United States. Italy (dashed-line)") +
+  ylab("Total confirmed deaths (certainly an undercount)") +
+  xlab("Days since 100 confirmed cases")
+
+ggsave(file = "output/coronavirus_deaths_compared_to_italy.png", plot = deaths_v_italy, height = 6, width = 9, dpi = 600)
+
+#########################################################################################################
+# Cases Make time trend of each country v USA on one plot
+cases_all <- ggplot(data = analysis, aes(x = days_since, y = total_to_date_confirmed, group = Country.Region, label = Country.Region)) +
+  geom_line(data = subset(analysis, Country.Region != "US"), alpha = .33, size = 3.5, color = "black") +
+  geom_line(data = subset(analysis, Country.Region == "US"), alpha = 1, size = 4, color = "black") +
+  geom_line(data = subset(analysis, Country.Region == "US"), alpha = 1, size = 3.5, color = "orange") +
+  theme_classic() +
+  ggtitle("The United States (in orange) looks more like Europe or Iran than Asia") +
+  ylab("Total confirmed cases (certainly an undercount)") +
+  xlab("Days since 100 confirmed cases") +
+  geom_label_repel(data = subset(analysis, max_days == days_since))
+
+ggsave(file = "output/coronavisrus_cases_all_on_one_plot.png", plot = cases_all, height = 6, width = 9, dpi = 600)
+
+
+# Deaths: Make time trend of each country v USA on one plot
+deaths_all <- ggplot(data = analysis, aes(x = days_since, y = total_to_date_deaths, group = Country.Region, label = Country.Region)) +
+  geom_line(data = subset(analysis, Country.Region != "US"), alpha = .33, size = 3.5, color = "black") +
+  geom_line(data = subset(analysis, Country.Region == "US"), alpha = 1, size = 4, color = "black") +
+  geom_line(data = subset(analysis, Country.Region == "US"), alpha = 1, size = 3.5, color = "red") +
+  theme_classic() +
+  ggtitle("Death count: The United States (in red) v other countries") +
+  ylab("Total deaths (certainly an undercount)") +
+  xlab("Days since 100 confirmed cases") +
+  geom_label_repel(data = subset(analysis, max_days == days_since))
+
+ggsave(file = "output/coronavirus_deaths_all_on_one_plot.png", plot = deaths_all, height = 6, width = 9, dpi = 600)
